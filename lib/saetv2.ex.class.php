69d295e8b2475416d465a1d4f005c96e49faec43
Elmer Zhang <freeboy6716@gmail.com>
2012-04-10 11:01:25 +0800

    添加 SaeTClientV2::set_debug() 方法 添加 SaeTClientV2::set_remote_ip() 方法 修复 请求接口时可能报 SSL: certificate subject name does not match 的BUG

--- lib/saetv2.ex.class.php
+++ lib/saetv2.ex.class.php
@@ -354,6 +354,7 @@ class SaeTOAuthV2 {
 		curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ci, CURLOPT_ENCODING, "");
 		curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
 		curl_setopt($ci, CURLOPT_HEADERFUNCTION, array($this, 'getHeader'));
 		curl_setopt($ci, CURLOPT_HEADER, FALSE);
 
@@ -375,7 +376,17 @@ class SaeTOAuthV2 {
 		if ( isset($this->access_token) && $this->access_token )
 			$headers[] = "Authorization: OAuth2 ".$this->access_token;
 
-		$headers[] = "API-RemoteIP: " . $_SERVER['REMOTE_ADDR'];
+		if ( !empty($this->remote_ip) ) {
+			if ( defined('SAE_ACCESSKEY') ) {
+				$headers[] = "SaeRemoteIP: " . $this->remote_ip;
+			} else {
+				$headers[] = "API-RemoteIP: " . $this->remote_ip;
+			}
+		} else {
+			if ( !defined('SAE_ACCESSKEY') ) {
+				$headers[] = "API-RemoteIP: " . $_SERVER['REMOTE_ADDR'];
+			}
+		}
 		curl_setopt($ci, CURLOPT_URL, $url );
 		curl_setopt($ci, CURLOPT_HTTPHEADER, $headers );
 		curl_setopt($ci, CURLINFO_HEADER_OUT, TRUE );
@@ -389,10 +400,13 @@ class SaeTOAuthV2 {
 			echo "=====post data======\r\n";
 			var_dump($postfields);
 
-			echo '=====info====='."\r\n";
+			echo "=====headers======\r\n";
+			print_r($headers);
+
+			echo '=====request info====='."\r\n";
 			print_r( curl_getinfo($ci) );
 
-			echo '=====$response====='."\r\n";
+			echo '=====response====='."\r\n";
 			print_r( $response );
 		}
 		curl_close ($ci);
@@ -480,6 +494,39 @@ class SaeTClientV2
 	function __construct( $akey, $skey, $access_token, $refresh_token = NULL)
 	{
 		$this->oauth = new SaeTOAuthV2( $akey, $skey, $access_token, $refresh_token );
+	}
+
+	/**
+	 * 开启调试信息
+	 *
+	 * 开启调试信息后，SDK会将每次请求微博API所发送的POST Data、Headers以及请求信息、返回内容输出出来。
+	 *
+	 * @access public
+	 * @param bool $enable 是否开启调试信息
+	 * @return void
+	 */
+	function set_debug( $enable )
+	{
+		$this->oauth->debug = $enable;
+	}
+
+	/**
+	 * 设置用户IP
+	 *
+	 * SDK默认将会通过$_SERVER['REMOTE_ADDR']获取用户IP，在请求微博API时将用户IP附加到Request Header中。但某些情况下$_SERVER['REMOTE_ADDR']取到的IP并非用户IP，而是一个固定的IP（例如使用SAE的Cron或TaskQueue服务时），此时就有可能会造成该固定IP达到微博API调用频率限额，导致API调用失败。此时可使用本方法设置用户IP，以避免此问题。
+	 *
+	 * @access public
+	 * @param string $ip 用户IP
+	 * @return bool IP为非法IP字符串时，返回false，否则返回true
+	 */
+	function set_remote_ip( $ip )
+	{
+		if ( ip2long($ip) !== false ) {
+			$this->oauth->remote_ip = $ip;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
